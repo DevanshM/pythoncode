79794036094bb907ef7426753079b238163fe9fd
DevanshM <47470098+DevanshM@users.noreply.github.com>
2019-06-18 18:59:40 +0530

    Update add.py

--- add.py
+++ add.py
@@ -2,6 +2,6 @@ x,y=map(int,input("x,y").split())
 z=x+y
 print("*"*60)
 print("x ={x} ".center(60))
-print("y ={y} ".center(60)
-print("z ={z} ".center(60))_
+print("y ={y} ".center(60))
+print("z ={z} ".center(60))
 print("*"*60)
